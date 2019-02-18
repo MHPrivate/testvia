100db0d97441f51beed44ea5fe5f97629c880143
Martin Meadows <martin@netconfigs.com>
2019-02-18 15:46:51 +0000

    refinements to the baseline code

--- via.js
+++ via.js
@@ -1,4 +1,4 @@
 #! /usr/bin/env node-strict
-var main = require('./lib/running');
+require('./lib/running').running = undefined; // causes main.running set _true_ once running AND _false_ while terminating
 
-require('./lib/replify')(!process.stdin.isTTY);
+require('./lib/replify')(!process.stdin.isTTY); // starts either a console:repl OR a daemon:replify (/run/<main>.sock)
