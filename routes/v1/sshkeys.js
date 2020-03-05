8da03048c665ceac0b0bff49f2354128990d4ef7
Martin Meadows <martin@netconfigs.com>
2020-03-05 10:54:26 +0000

    adjustment to ssh-related permissions naming (ssh.all=>ssha, ssh=>ssho)

--- routes/v1/sshkeys.js
+++ routes/v1/sshkeys.js
@@ -9,7 +9,7 @@ exports.get('/', function (req, res, next) { // GET /vi/sshkeys
     var locals = req.locals;
     next.index = req.index;
     chain(next, function () {
-        mysql('select k.*,u.username from users u join userSkills s on s.userId=u.id join userSshs k on u.id=k.userId where s.skillName="ssh.all"', this);
+        mysql('select k.*,u.username from users u join userSkills s on s.userId=u.id join userSshs k on u.id=k.userId where s.skillName="ssha"', this);
 
     }, function (users, meta) { // {id,key,keyId,userId,username}
         locals.users = users;
@@ -24,7 +24,7 @@ exports.get('/:userId', function (req, res, next) { // GET /v1/sshkeys/:userId
     var locals = req.locals;
     next.index = req.index;
     chain(next, function () {
-        mysql('select k.*,u.username from users u join userSkills s on u.id = s.userId join userSshs k on u.id = k.userId where(s.skillName = "ssh.all" or (k.userId=? and s.skillName like "ssh%")) group by id', [req.params.userId], this);
+        mysql('select k.*,u.username from users u join userSkills s on u.id = s.userId join userSshs k on u.id = k.userId where(s.skillName = "ssha" or (k.userId=? and s.skillName="ssho")) group by id', [req.params.userId], this);
 
     }, function (users, meta) { // {id,key,keyId,userId,username}
         locals.users = users;
