ebde8f02ab1c3b5d5c3d2470ff95aad87a17c7fd
Martin Meadows <martin@netconfigs.com>
2020-04-26 16:46:05 +0000

    adjustments for freeswitch-tls certificate management

---
+++ root/etc/freeswitch/tls.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+cat=/usr/bin/cat
+chgrp=/usr/bin/chgrp
+curl=/usr/bin/curl
+dirname=/usr/bin/dirname
+fs_cli=/usr/bin/fs_cli
+ln=/usr/bin/ln
+openssl=/usr/bin/openssl
+rm=/usr/bin/rm
+check=dtls-srtp.pem
+token=8a947e04-c590-4f6c-b40c-455e385e2ef2
+
+umask 027
+cd $(dirname $0)
+[ -d tls ] || $ln -s /etc/pki/tls .
+cd tls
+
+old=$([ -e "$check" ] && openssl x509 -noout -serial -in $check)
+for pem in key cert chain; do
+    $curl -sH "Authorization: Bearer $token" https://localhost.appello.care/maintain/cert/appello.care?$pem >$$-$pem.pem
+done
+
+$cat $$-cert.pem $$-key.pem >agent.pem
+$cat $$-chain.pem >cafile.pem
+$cat $$-cert.pem $$-key.pem >dtls-srtp.pem #  just to be consistent though self-signed is likely suggicient
+$cat $$-cert.pem $$-chain.pem $$-key.pem >tls.pem
+$cat $$-cert.pem $$-chain.pem $$-key.pem >wss.pem
+
+$rm -f $$-*.pem
+$chgrp daemon agent.pem cafile.pem dtls-srtp.pem tls.pem wss.pem
+
+[ "$old" == "$(openssl x509 -noout -serial -in $check)" ]  && exit
+$fs_cli -x 'reload mod_sofia'
