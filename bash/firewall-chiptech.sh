0988ad602a71dd34e9badd039cad529fca89dbc4
Martin <martin@netconfigs.com>
2020-09-30 11:19:29 +0000

    fixed worker exception on config'd e164s + refactored firewall-*.sh scripts

--- bash/firewall-chiptech.sh
+++ bash/firewall-chiptech.sh
@@ -1,7 +1,8 @@
-while read cidr other; do
-echo $cidr:
-firewall-cmd --ipset=chums4 --add-entry=$cidr
+while read; do
+	cidr=${REPLY%%[$'\t' ]*}
+	[ -z "$cidr" ] && continue
+	echo firewall-cmd $@ --ipset=chums4 --add-entry=$cidr
 done <<EOT
-125.236.214.19/32
-122.61.175.170/32
+125.236.214.19/32	sean hadley dev
+122.61.175.170/32	sean hadley dev
 EOT
--- bash/firewall-scaip.sh
+++ bash/firewall-scaip.sh
@@ -1,16 +1,17 @@
-while read cidr other; do
-echo $cidr:
-firewall-cmd --perm --ipset=chums4 --add-entry=$cidr
+while read; do
+        cidr=${REPLY%%[$'\t' ]*}
+        [ -z "$cidr" ] && continue
+        echo firewall-cmd $@ --ipset=chums4 --add-entry=$cidr
 done <<EOT
+178.22.139.77/32    lon.out.simwood.com
 185.63.140.77/32    slo.out.simwood.com
 185.63.142.77/32    man.out.simwood.com
-178.22.139.77/32    lon.out.simwood.com
-85.133.122.73/32    essence + chiptech
 195.10.99.99/32     chiptech
-85.214.144.155/32   gw1.telealarm.com
-94.23.55.219/32     gw2.telealarm.com
-81.171.12.244/32    gw3.telealarm.com
 217.156.234.163/32  neatnovo
+81.171.12.244/32    gw3.telealarm.com
+85.133.122.73/32    essence + chiptech
+85.214.144.155/32   gw1.telealarm.com
+ 87.86.190.25	    careip via CSL
+94.23.55.219/32     gw2.telealarm.com
 EOT
 
-#87.86.190.25	    careip via CSL
