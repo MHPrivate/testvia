2d4ed0e481d7e95f5025d7310138aa37318aabbe
Martin <martin@netconfigs.com>
2020-10-15 16:08:34 +0000

    updated firewalling note form mobius sources + rename recording file while while recording

--- bash/firewall-scaip.sh
+++ bash/firewall-scaip.sh
@@ -7,7 +7,9 @@ done <<EOT
 185.63.140.77/32    slo.out.simwood.com
 185.63.142.77/32    man.out.simwood.com
 195.10.99.99/32     chiptech
-217.156.234.163/32  neatnovo
+217.156.234.130/32  neatnovo (mobius-additional)
+217.156.234.160/29  neatnovo (mobius-primary)
+217.156.234.176/29  neatnovo (mobius-secondary)
 81.171.12.244/32    gw3.telealarm.com
 85.133.122.73/32    essence + chiptech
 85.214.144.155/32   gw1.telealarm.com
