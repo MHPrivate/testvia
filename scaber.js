7c4abb58bd7dfcc363da0e76dba8844a2e98d267
Martin <martin@netconfigs.com>
2022-05-20 17:00:49 +0000

    housekeeping

--- scaber.js
+++ scaber.js
@@ -70,7 +70,6 @@ if (cluster.isMaster) {
         nowip: require('./lib/nowip'),
         rpscb: require('./lib/rpscb'),
         Session: require('./lib/scaber/session'),
-        //Tasks: require('./lib/scaber/tasks'),
         worker: require('./lib/scaber/worker'),
     });
 }
