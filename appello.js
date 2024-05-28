f8ff86b8419fc6a69e3fde5a4b08faf4a9e1b3a4
Mark Hall <mark@postbag.net>
2024-05-28 13:59:38 +0100

    develop-aws: Adjust processing if in AWS. appello.js - add new aws.js to main.modules. nsupdate.js - ns changes via Route53 if in AWS. proxyConfig - fetch AWS ext IP if needed.

--- appello.js
+++ appello.js
@@ -17,6 +17,7 @@ var main = Object.defineProperties(Object.assign(exports,{
     hack: {},       // diagnostic runtime settings - usually empty
     identity: null, // typically https://<hostname>:<port> of this service instance
     modules: { // only for diagnostic accessibility - not to be used by code - use require(s) only
+        aws: require('./lib/aws'),
         esl: require('./lib/esl'),
         fsxml: require('./lib/fsxml'),
         harvest: require('./lib/harvest'),
